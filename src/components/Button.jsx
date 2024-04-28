1940288bd073377fb6a1d683bf55f8a7c4c7f384
Farahksyu <ksyu.farah@gmail.com>
2024-04-28 19:26:16 +0700

    added button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-
 export const Button = () => {
     // Farah
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      id="submit"
+      type="submit"
+      className={`${style} font-semibold px-4 py-2 w-full rounded-md shadow-md transition-all duration-500`}
+      onClick={onClick}>
+      {nama}
+    </button>
+  );
+};
