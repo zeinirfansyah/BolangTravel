fad56481ea12c9f7d48d5e5254b58cd6213489a7
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-29 15:13:44 +0700

    refactor [App.jsx]: added /payment and /completed as routes endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { About } from "./pages/about/About";
 import { TravelPackage } from "./pages/travelPackages/TravelPackage";
 import { BookingProgress } from "./pages/bookingProgess/BookingProgress";
 import { Layout } from "./layouts/Layout";
+import { Payment } from "./pages/bookingProgess/Payment";
+import { Completed } from "./pages/bookingProgess/Completed";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/paket-wisata" element={<TravelPackage />} />
           <Route path="/detail-paket/:id" element={<TravelPackage />} />
           <Route path="/booking-progres" element={<BookingProgress />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/completed" element={<Completed />} />
         </Routes>
       </Layout>
     </>
