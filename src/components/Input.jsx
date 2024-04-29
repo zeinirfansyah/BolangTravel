42a676c4387dbd7f93595e1dab41c74f8686ecde
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-29 15:22:49 +0700

    refactor [Input.jsx]: added type props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
-export const Input = ({ name, placeholder, onChange}) => {
+export const Input = ({ name, type, placeholder, onChange}) => {
   return (
     <>
       <input
         name={name}
+        type={type}
         placeholder={placeholder}
         onChange={onChange}
         className="border-none bg-[#F5F6F8] rounded-lg p-4 w-full focus:outline-secondary focus:ring-0 focus:bg-white transition-all duration-500"
@@ -15,6 +16,7 @@ export const Input = ({ name, placeholder, onChange}) => {
 
 Input.propTypes = {
   name: PropTypes.string,
+  type: PropTypes.string,
   placeholder: PropTypes.string,
   onChange: PropTypes.func
 }
