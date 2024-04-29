09a1477f2e1eb1babee5940342c95fbb6bf4cc10
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-29 15:40:59 +0700

    refactor [Input.jsx]: added accepted file type props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-export const Input = ({ name, type, placeholder, onChange}) => {
+export const Input = ({ name, type, accept, placeholder, onChange}) => {
   return (
     <>
       <input
@@ -8,6 +8,7 @@ export const Input = ({ name, type, placeholder, onChange}) => {
         type={type}
         placeholder={placeholder}
         onChange={onChange}
+        accept={accept}
         className="border-none bg-[#F5F6F8] rounded-lg p-4 w-full focus:outline-secondary focus:ring-0 focus:bg-white transition-all duration-500"
       />
     </>
@@ -17,6 +18,7 @@ export const Input = ({ name, type, placeholder, onChange}) => {
 Input.propTypes = {
   name: PropTypes.string,
   type: PropTypes.string,
+  accept: PropTypes.string,
   placeholder: PropTypes.string,
   onChange: PropTypes.func
 }
