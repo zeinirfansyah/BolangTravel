7dfe2e38ddfde4189abd1675bcb3224c70b30b8e
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-04-29 14:48:33 +0700

    refactor [Header.jsx]: fixed routes on Link and fixed toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="sticky top-0 z-10 bg-white shadow">
       <div className="max-w-7xl mx-auto">
         <nav className="Navbar md:flex justify-start gap-20 items-center py-6 mx-4">
           <ul className="flex items-center justify-between">
             <li>
-              <Link id="brand" href="index.html">
+              <Link id="brand" to="index.html">
                 <h1 className="text-xl font-semibold  transition-all duration-500">
-                  <span className="text-[#00C0E6]">Bolang</span>Travel.
+                  <span className="text-secondary">Bolang</span>Travel.
                 </h1>
               </Link>
             </li>
             <button
-              className="sm:hidden hover:bg-white px-3 py-1 transition-all rounded-lg cursor-pointer shadow"
-              onClick="toggleMenu()"
+              className="sm:hidden hover:bg-white px-3 py-1 transition-all  duration-500 rounded-lg cursor-pointer shadow"
+              onClick={toggleMenu}
               type="button"
             >
               <span className="text-xl">&#9776;</span>
@@ -23,36 +30,38 @@ export const Header = () => {
           </ul>
           <ul
             id="menu"
-            className="md:flex gap-5 hidden transition-all duration-500"
+            className={`md:flex gap-5 ${
+              isMenuOpen ? "" : "hidden"
+            } text-navy`}
           >
             <li className="my-5 md:my-0">
               <Link
-                href="index.html"
-                className="hover:text-[#00C0E6] transition-all duration-500"
+                to="/"
+                className="hover:text-secondary transition-all duration-500"
               >
                 Home
               </Link>
             </li>
             <li className="my-5 md:my-0">
               <Link
-                href="TravelPackages.html"
-                className="active hover:text-[#00C0E6] transition-all duration-500"
+                to="/paket-wisata"
+                className="active hover:text-secondary transition-all duration-500"
               >
                 Paket Wisata
               </Link>
             </li>
             <li className="my-5 md:my-0">
               <Link
-                href="about.html"
-                className="hover:text-[#00C0E6] transition-all duration-500"
+                to="/tentang-kami"
+                className="hover:text-secondary transition-all duration-500"
               >
                 Tentang Kami
               </Link>
             </li>
             <li className="my-5 md:my-0">
               <Link
-                href="#footer"
-                className="hover:text-[#00C0E6] transition-all duration-500"
+                to="#footer"
+                className="hover:text-secondary transition-all duration-500"
               >
                 Kontak
               </Link>
