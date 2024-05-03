4b8e1c37425ba8229737bb5e3d3342e9e185436d
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-03 15:31:25 +0700

    refactor [Input]: changed style border

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -9,7 +9,7 @@ export const Input = ({ name, type, accept, placeholder, onChange}) => {
         placeholder={placeholder}
         onChange={onChange}
         accept={accept}
-        className="border-none bg-[#F5F6F8] rounded-lg p-4 w-full focus:outline-secondary focus:ring-0 focus:bg-white transition-all duration-500"
+        className="border bg-[#F5F6F8] rounded-lg p-4 w-full focus:outline-secondary focus:ring-0 focus:bg-white transition-all duration-500"
       />
     </>
   );
