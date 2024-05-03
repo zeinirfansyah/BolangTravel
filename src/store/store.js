1deac1e7b6489cb552b24c235b49f390a4d8edeb
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-03 15:15:35 +0700

    feat [store]: added fetchProduct and ProductDetails state

---
+++ src/store/store.js
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import data from "../data/data.json";
+
+const initialState = {
+  isLoading: true,
+  products: [],
+  product: [],
+};
+
+const useStore = create(
+  persist(
+    (set) => ({
+      ...initialState,
+
+      fetchProducts: async () => {
+        try {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          set({
+            isLoading: false,
+            products: data,
+          });
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      },
+
+      ProductDetails: (p) => {
+        set({ Product: p });
+      },
+    }),
+    {
+      name: "bolangTravelStore",
+      getStorage: () => localStorage,
+    }
+  )
+);
+
+export default useStore;
