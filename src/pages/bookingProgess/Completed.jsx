0dcb7cb5234e6805e840ebf7f93b20706d7f90d2
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-04 19:02:09 +0700

    refactor [Completed.jsx]: changed copywrite language

--- src/pages/bookingProgess/Completed.jsx
+++ src/pages/bookingProgess/Completed.jsx
@@ -26,13 +26,13 @@ export const Completed = () => {
               </span>
             </div>
             <h1 className="text-2xl lg:text-3xl font-bold text-center">
-              Completed!
+              Selesai!
             </h1>
           </div>
 
           <div
             data-aos="zoom-in"
-            className="w-full flex flex-col justify-center items-center gap-5"
+            className="w-full flex flex-col justify-center items-center gap-3"
           >
             <img
               src={completed_illustration}
@@ -40,8 +40,10 @@ export const Completed = () => {
               className="w-[300px] md:w-[300px]"
             />
             <h2 className="text-l lg:text-xl text-center text-darkGray">
-              We will inform you via email later once the transaction has been
-              accepted
+              Kami akan melakukan validasi pembayaran anda! 
+            </h2>
+            <h2 className="text-l lg:text-xl text-center text-darkGray">
+             Kami akan mengirimkan notifikasi ke email anda jika pembayaran telah diterima. 
             </h2>
           </div>
           <div className="flex justify-center items-center gap-3 w-full lg:w-1/4">
@@ -49,7 +51,7 @@ export const Completed = () => {
               to="/"
               className="button bg-secondary text-white px-6 py-2 rounded w-full text-center font-semibold transition-all duration-500 hover:bg-white hover:text-secondary hover:border-secondary border-2 border-secondary"
             >
-              Back to Home
+              Kembali ke Beranda
             </Link>
           </div>
         </div>
