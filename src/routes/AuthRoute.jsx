b22326e23bbed23b857217bad6ee3dc115b19d2c
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-10 23:09:05 +0700

    feat [AuthRoute.jsx]: added protected route named AuthRoute.jsx to protect authenticated route

---
+++ src/routes/AuthRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from "react-router-dom";
+import useAuthStore from "../store/useAuthStore";
+
+const AuthRoute = ({
+  role,
+}) => {
+  const { isAuthenticated, user } = useAuthStore((state) => state);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (user.role !== role) {
+    return <Navigate to="/not-found" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthRoute;
